d5d1a901a8acb93e48cf139705abd535eb4a7ea4
Kapil Sharma <kapil19092003@gmail.com>
2024-08-31 18:31:02 +0530

    31staugust - added separation of concerns in models and created owner models

---
+++ models/owner-model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://127.0.0.1:27017/Scatch");
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        minLength : 3,
+        trim: true,
+    },
+    email : String,
+    password : String,
+    product: {
+        type : Array,
+        default : []
+    },
+    picture : String,
+    gstin: String,
+});
+
+module.exports =  mongoose.model("owner", ownerSchema);
